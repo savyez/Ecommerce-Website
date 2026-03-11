56bc665dd6ae4af2ab6a634c18b0ee539cdaa29e
Savyez <visnuk252@gmail.com>
2026-03-11 12:17:01 +0530

    Feat(backend): DELETE product by ID

--- backend/controller/productController.js
+++ backend/controller/productController.js
@@ -41,3 +41,19 @@ export const updateProduct = async (req, res) => {
     })
 }
 
+
+//  Delete Product
+export const deleteProduct = async (req, res) => {
+    let product = await Product.findById(req.params.id);
+    if (!product) {
+        return res.status(500).json({
+            success: false,
+            message: "Product Not Found"
+        })
+    }
+    product = await Product.findByIdAndDelete(req.params.id)
+    res.status(200).json({
+        success: true,
+        message: "Product Deleted Successfully"
+    })
+}
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,11 +1,11 @@
 import express from "express";
-import { createProducts, getAllProducts, updateProduct } from "../controller/productController.js";
+import { createProducts, deleteProduct, getAllProducts, updateProduct } from "../controller/productController.js";
 
 const router = express.Router();
 
 
 // Routes
 router.route("/products").get(getAllProducts).post(createProducts);
-router.route("/product/:id").put(updateProduct);
+router.route("/product/:id").put(updateProduct).delete(deleteProduct);
 
 export default router;
