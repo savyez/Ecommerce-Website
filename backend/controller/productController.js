8768b9e54d578d935d108eeae22ed6ffd12288b4
Savyez <visnuk252@gmail.com>
2026-03-11 12:06:34 +0530

    Feat(backend): Add Update route and Update product by ID

--- backend/controller/productController.js
+++ backend/controller/productController.js
@@ -11,10 +11,33 @@ export const createProducts = async(req, res) => {
 }
 
 
-
-export const getAllProducts = (req,res) => {
+// Get all Products
+export const getAllProducts = async (req,res) => {
+    const products = await Product.find()
     res.status(200)
         .json({
-            message: "All products"
+            success:true,
+            products
         })
 }
+
+
+// Update product
+export const updateProduct = async (req, res) => {
+    let product = await Product.findById(req.params.id);
+    if (!product) {
+        return res.status(500).json({
+            success: false,
+            message: "Product Not Found"
+        })
+    }
+    product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+        runValidators: true
+    })
+    res.status(200).json({
+        success: true,
+        product
+    })
+}
+
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,11 +1,11 @@
 import express from "express";
-import { createProducts, getAllProducts } from "../controller/productController.js";
+import { createProducts, getAllProducts, updateProduct } from "../controller/productController.js";
 
 const router = express.Router();
 
 
 // Routes
 router.route("/products").get(getAllProducts).post(createProducts);
-
+router.route("/product/:id").put(updateProduct);
 
 export default router;
