ef1c71d801aec4851d0d78f6461225ba984b9460
Savyez <visnuk252@gmail.com>
2026-03-11 21:29:24 +0530

    feat(auth): verify JWT from cookies and protect product routes

--- backend/app.js
+++ backend/app.js
@@ -2,11 +2,13 @@ import express from "express";
 import product from "./routes/productRoutes.js";
 import user from "./routes/userRoutes.js";
 import errorHandleMiddleware from "./middleware/error.js";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
 // Middleware
 app.use(express.json())
+app.use(cookieParser())
 
 // Route
 app.use("/api/v1/", product)
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,12 +1,13 @@
 import express from "express";
 import { createProducts, deleteProduct, getAllProducts, getSingleProduct, updateProduct } from "../controller/productController.js";
+import { verifyUserAuth } from "../middleware/userAuth.js";
 
 const router = express.Router();
 
 
 // Routes
 router.route("/products")
-.get(getAllProducts)
+.get(verifyUserAuth, getAllProducts)
 .post(createProducts);
 
 
